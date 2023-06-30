152cb2d01f4a1a907772af97b38d3b863faeef3b
Daniela <116597316+Daniela-ZS@users.noreply.github.com>
2023-06-30 15:39:00 -0300

    Add files via upload

---
+++ Python/Clase01_variablesydatos/Homework_01.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Variables
+
+# 1) Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla
+
+# In[7]:
+
+
+
+
+# 2) Imprimir el tipo de dato de la constante 8.5
+
+# In[3]:
+
+
+
+
+
+# 3) Imprimir el tipo de dato de la variable creada en el punto 1
+
+# In[8]:
+
+
+
+
+
+# 4) Crear una variable que contenga tu nombre
+
+# In[2]:
+
+
+
+
+# 5)Mostrar el tipo de dato de la variable creada en el punto 4
+
+# In[4]:
+
+
+
+
+
+# 6) Crear una variable que contenga el valor del número Pi redondeado a 4 decimales
+
+# In[1]:
+
+
+
+
+
+# 7) Crear una variable que contenga el valor 'True' y otra que contenga el valor True. ¿Se trata de lo mismo?
+
+# In[3]:
+
+
+
+
+
+# 8) Imprimir el tipo de dato correspondientes a las variables creadas en el punto 7
+
+# In[5]:
+
+
+
+
+
+# 9) Asignar a una variable, la suma de un número entero y otro decimal
+
+# In[1]:
+
+
+
+
+
+# 10) Realizar una operación de multiplicación
+
+# In[5]:
+
+
+
+
+
+# 11) Mostrar el resultado de elevar 2 a la octava potencia
+
+# In[6]:
+
+
+
+
+# 12) Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla
+
+# In[8]:
+
+
+
+
+
+# 13) De la división anterior solamente mostrar la parte entera
+
+# In[9]:
+
+
+
+
+
+# 14) De la división de 27 entre 4 mostrar solamente el resto
+
+# In[1]:
+
+
+
+
+
+# 15) Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 13 y 14. Obtener 27 como resultado
+
+# In[2]:
+
+
+
+
+
+# 16) Utilizar el operador "+" en una operación donde intervengan solo variables alfanuméricas
+
+# In[3]:
+
+
+
+
+
+# 17) Evaluar si "2" es igual a 2. ¿Por qué ocurre eso?
+
+# In[4]:
+
+
+
+
+
+# 18) Utilizar las funciones de cambio de tipo de dato, para que la validación del punto 17 resulte verdadera
+
+# In[11]:
+
+
+
+
+
+# 19) ¿Por qué arroja error el siguiente cambio de tipo de datos? a = float('3,8')
+
+# In[12]:
+
+
+
+
+
+# 20) Crear una variable con el valor 3, y utilizar el operador '-=' para modificar su contenido y que de como resultado 2.
+
+# In[15]:
+
+
+
+
+
+# 21) Realizar la operación 2 + '2' ¿Por qué no está permitido? ¿Si los dos operandos serían del mismo tipo, siempre arrojaría el mismo resultado?
+
+# In[23]:
+
+
+
+
+
+
+# 22) Realizar una operación válida entre valores de tipo entero y string
+
+# In[30]:
+
+
+
---
+++ Python/Clase02_flujosdecontrol/Homework_02.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Flujos de Control
+
+# 1) Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla si es mayor o menor a cero
+
+# In[4]:
+
+
+
+
+
+# 2) Crear dos variables y un condicional que informe si son del mismo tipo de dato
+
+# In[5]:
+
+
+
+
+
+# 3) Para los valores enteros del 1 al 20, imprimir por pantalla si es par o impar
+
+# In[7]:
+
+
+
+
+
+# 4) En un ciclo for mostrar para los valores entre 0 y 5 el resultado de elevarlo a la potencia igual a 3
+
+# In[9]:
+
+
+
+
+
+# 5) Crear una variable que contenga un número entero y realizar un ciclo for la misma cantidad de ciclos
+
+# In[10]:
+
+
+
+
+
+# 6) Utilizar un ciclo while para realizar el factoreo de un número guardado en una variable, sólo si la variable contiene un número entero mayor a 0
+
+# In[33]:
+
+
+
+
+
+# 7) Crear un ciclo for dentro de un ciclo while
+
+# In[38]:
+
+
+
+
+
+# 8) Crear un ciclo while dentro de un ciclo for
+
+# In[3]:
+
+
+
+
+
+# 9) Imprimir los números primos existentes entre 0 y 30
+
+# In[54]:
+
+
+
+
+# 10) ¿Se puede mejorar el proceso del punto 9? Utilizar las sentencias break y/ó continue para tal fin
+
+# In[55]:
+
+
+
+
+
+# 11) Aplicando continue, armar un ciclo while que solo imprima los valores divisibles por 12, dentro del rango de números de 100 a 300
+
+# In[62]:
+
+
+
+
+
+# 12) Crear un ciclo while que encuentre dentro del rango de 100 a 300 el primer número divisible por 3 y además múltiplo de 6
+
+# In[75]:
+
+
+
